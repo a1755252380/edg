d7b8382b96bab09371a06ccdeb275b186e50d806
linan <1755252380@qq.com>
2021-11-06 00:14:40 +0800

    edg战旗

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <remote-js src="https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/three.js/72/three.min.js"></remote-js>
+    <router-view />
   </div>
 </template>
+<script>
+export default {
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    'remote-js': {
+      render (createElement) {
+        return createElement('script', { attrs: { type: 'text/javascript', src: this.src } });
+      },
+      props: {
+        src: { type: String, required: true },
+      },
+    }
+  },
 
+}
+</script>
 <style lang="scss">
+@import url("../src/assets/html.css");
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,169 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <div id="left">
+        <div class="meter">
+          <p>color</p>
+          <input type="color"
+                 id="color"
+                 value="#ffffff"
+                 onclick="setColor()">
+        </div>
+        <div class="meter">
+          <p>image</p>
+          <input type="file"
+                 id="texture"
+                 accept="image/png,image/jpeg,image/gif"
+                 onchange="loadTexture(this.files)">
+        </div>
+        <div class="meter">
+          <p>camera</p>
+          <input type="range"
+                 id="angle"
+                 value="-24"
+                 min="-180"
+                 max="180"
+                 oninput="setCamera()">
+          <span id="textAngle">[0°]</span>
+        </div>
+        <div class="meter">
+          <p>horizontal</p>
+          <input type="range"
+                 id="h"
+                 value="0.5"
+                 min="0"
+                 max="1.0"
+                 step="0.1">
+        </div>
+        <div class="meter">
+          <p>vertical</p>
+          <input type="range"
+                 id="v"
+                 value="0.3"
+                 min="0"
+                 max="1.0"
+                 step="0.1">
+        </div>
+        <div class="meter">
+          <p>swing</p>
+          <input type="range"
+                 id="w"
+                 value="0.2"
+                 min="0"
+                 max="1.0"
+                 step="0.1">
+        </div>
+        <div class="meter">
+          <p>speed</p>
+          <input type="range"
+                 id="s"
+                 value="0.5"
+                 min="0"
+                 max="1.0"
+                 step="0.1">
+        </div>
+        <div class="meter">
+          <input type="button"
+                 value="UNDO"
+                 onclick="initFlag()">
+        </div>
+      </div>
+      <div id="right">
+        <div id="renderArea"></div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+
+import * as THREE from 'three'
+import "../assets/html.js"
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  mounted () {
+    $("body").css("overflow", "hidden")
+
+  },
+  destroyed () {
+    $("body").css("overflow", "")
   }
 }
 </script>
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  main {
+    display: flex;
+  }
+  .meter {
+    display: flex;
+    width: 80%;
+    margin: 15px auto;
+  }
+  .meter p {
+    width: 120px;
+    margin: 0;
+  }
+  .meter span {
+    width: 40px;
+    margin: 0 15px;
+    line-height: 12px;
+  }
+  #left {
+    width: 50%;
+    display: none;
+  }
+  #right {
+    width: 100%;
+    text-align: center;
+  }
+
+  h1 {
+    width: 100%;
+    font-size: 48px;
+    text-align: center;
+  }
+
+  input[type="range"] {
+    -webkit-appearance: none;
+    background-color: #cccccc;
+    height: 10px;
+    overflow: visible;
+    width: 300px;
+    position: relative;
+    border-radius: 3px;
+  }
+
+  input[type="range"]::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    background-color: #333333;
+    box-shadow: 1px 1px 8px #999999;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    position: relative;
+    z-index: 100;
+  }
+
+  input[type="range"]::-webkit-slider-thumb:before {
+    content: "";
+    position: absolute;
+    top: 100%;
+    left: 40%;
+    width: 10%;
+    height: 200px;
+    display: block;
+    background: red;
+    z-index: -10;
+    box-shadow: 0 0 10px darkred;
+    opacity: 0.1;
+  }
+
+  input[type="range"]:hover::-webkit-slider-thumb:before {
+    opacity: 1;
+  }
+}
+</style>
